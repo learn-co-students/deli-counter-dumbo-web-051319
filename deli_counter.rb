b3a59dff9e3962b26baba6ed4146aeb1e55cc825
Lorena Ramirez-Lopez <daleloreny@gmail.com>
2019-05-02 00:16:37 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,5 @@
 # Write your code here.
-# Build a method that a new customer will use when entering the deli. The take_a_number method should accept two arguments, the array for the current line of people (katz_deli), and a string containing the name of the person joining the end of the line. The method should call out (puts) the person's name along with their position in line. Top-Tip: Remember that people like to count from 1, not from 0 ("zero") like computers.
+
 require "pry"
 
 katz_deli = []
@@ -16,3 +16,11 @@ def line(deli)
   end
 end
 
+# Build a method that a new customer will use when entering the deli. The take_a_number method should accept two arguments, the array for the current line of people (katz_deli), and a string containing the name of the person joining the end of the line. The method should call out (puts) the person's name along with their position in line. Top-Tip: Remember that people like to count from 1, not from 0 ("zero") like computers.
+
+def take_a_number(deli, next_person)
+  deli << next_person
+  puts "Welcome, #{next_person}. You are number #{next_person[i+1]} in line."
+  
+end
+  
