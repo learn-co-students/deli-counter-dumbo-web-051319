6f12239ef40b27b9f7a97716285988daa3394c5a
Lorena Ramirez-Lopez <daleloreny@gmail.com>
2019-05-02 03:51:47 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -30,5 +30,10 @@ def now_serving()
     puts "There is nobody waiting to be served!"
   
   else
-    
+    current_line = "The line is currently:"
+    deli.each.with_index (1) do |name, i|
+      current_line << " #{i}. #{name}"
+    end
+    puts current_line
+  end
 end
